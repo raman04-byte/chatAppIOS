38006dc3d071b00accbc4e6c2e475c6bce59911e
Raman Tank <ramantank04022002@gmail.com>
2024-10-04 13:19:17 +0530

    Initial Commit

---
+++ ChatApplication/ChatApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatApplicationApp.swift
+//  ChatApplication
+//
+//  Created by Raman Tank on 04/10/24.
+//
+
+import SwiftUI
+
+@main
+struct ChatApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatApplicationTests/ChatApplicationTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChatApplicationTests.swift
+//  ChatApplicationTests
+//
+//  Created by Raman Tank on 04/10/24.
+//
+
+import Testing
+@testable import ChatApplication
+
+struct ChatApplicationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
